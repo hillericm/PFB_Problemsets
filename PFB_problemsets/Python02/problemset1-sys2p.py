2ccc4efbad890ea4374404a4f40d5440aae12e3b
ehi <ehi@pfb04.cshl.edu>
2018-10-16 11:45:54 -0400

    added comment about + versus ,

--- PFB_problemsets/Python02/problemset1-sys2p.py
+++ PFB_problemsets/Python02/problemset1-sys2p.py
@@ -5,7 +5,7 @@ myName=sys.argv[1]
 favColor=sys.argv[2]
 favSport=sys.argv[3]
 favAnimal=sys.argv[4]
-print('My name is', myName ,'\n' +
+print('My name is', myName ,'\n' + # comma adds a space between strings, plus sign does not add  a space between strings
 'My favorite color is', favColor , '\n' +
 'My favorite sport is', favSport, '\n' +
 'My favorite animal is', favAnimal)
