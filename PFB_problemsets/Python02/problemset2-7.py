84e83e1b491ddcb0bb93c70e9cd20ef046b0b52f
ehi <ehi@pfb04.cshl.edu>
2018-10-16 16:08:33 -0400

    add problemset2-7 and 2-8.py files

---
+++ PFB_problemsets/Python02/problemset2-7.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+import sys
+
+TotalPenguin=sys.argv[1]
+
+if int(TotalPenguin) > 29:
+  print('Alright alright alright')
+else:
+  print('No dice, compadre')
---
+++ PFB_problemsets/Python02/problemset2-8.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+import sys
+
+TotalPenguin=sys.argv[1]
+
+if int(TotalPenguin) > 0 :
+  print('Alright alright alright. A positive number')
+else:
+  print('No dice, compadre. That one is negative.')
