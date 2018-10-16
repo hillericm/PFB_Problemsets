6b5c49c9fb87f9b893e8e43e99e60dc14c82c1fb
ehi <ehi@pfb04.cshl.edu>
2018-10-16 17:05:49 -0400

    fix large positive number indivisible by 3

--- PFB_problemsets/Python02/problemset2-12.py
+++ PFB_problemsets/Python02/problemset2-12.py
@@ -1,16 +1,19 @@
 #!/usr/bin/env python3
 import sys
 
-TestingNumber=int(sys.argv[1])
+TestingNumber=float(sys.argv[1])
 
 if TestingNumber > 0 :
   if TestingNumber < 50 :
     if TestingNumber % 2 == 0:
-      print('Alright alright alright. A positive even number smaller than 50.') 
+      print('Alright alright alright. A positive even number smaller than 50.')
     else:
-      print('Close, but no one wants an odd positive number')
-  elif TestingNumber % 3 == 0:
-    print('A really big positive number. And divisible by 3. Well done.')
+      print('Close, but no one wants a small odd positive number')
+  elif TestingNumber >50:
+    if TestingNumber % 3 == 0:
+      print('A really big positive number. And divisible by 3. Well done.')
+    elif TestingNumber % 3 > 0:
+      print('A large positive number indivisible by 3? Boring')
   else:
     print('Nifty Fifty')
 
