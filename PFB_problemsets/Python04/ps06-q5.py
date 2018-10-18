1233dc6be2cf25028b4ff085ff23724a0ed526e7
ehi <ehi@pfb04.cshl.edu>
2018-10-18 19:25:16 -0400

    finished ps6 except for bonus

--- PFB_problemsets/Python04/ps06-q5.py
+++ PFB_problemsets/Python04/ps06-q5.py
@@ -3,11 +3,14 @@
 allgenes = open('alpaca_all_genes.tsv','r')
 stemgenes = open('alpaca_stemcellproliferation_genes.tsv','r')
 piggenes = open('alpaca_pigmentation_genes.tsv','r')
+tfgenes = open('alpaca_tf_genes.tsv', 'r')
+tfstem = open('alpaca_tfstem_genes.tsv', 'w')
 nonstem = open('alpaca_nonstem_genes.tsv','w')
 pigstem = open('alpaca_pigstem_genes.tsv','w')
 all_li = []
 stem_li = []
 pig_li = []
+tf_li = []
 
 for line in allgenes:
 	line = line.rstrip()
@@ -24,12 +27,31 @@ for line in piggenes:
 	pig_li.append(line)
 pig_li.pop(0)
 
+for line in tfgenes:
+	line = line.rstrip()
+	tf_li.append(line)
+tf_li.pop(0)
+
+
 set_all = set(all_li)
 set_stem = set(stem_li)
+set_tf = set(tf_li)
 set_pig = set(pig_li)
 
-nonstem = set_all - set_stem
-pigstem = set_pig & set_stem
+nonstem_comp = set_all - set_stem
+pigstem_comp = set_pig & set_stem
+tfstem_comp = set_tf & set_stem
 
-print(nonstem)
-print(pigstem)
+for entry in tfstem_comp:
+	tfstem.write(entry)
+	tfstem.write('\n')
+
+for entry in nonstem_comp:
+	nonstem.write(entry)
+	nonstem.write('\n')
+
+
+for entry in pigstem_comp:
+	pigstem.write(entry)
+	pigstem.write('\n')
+
