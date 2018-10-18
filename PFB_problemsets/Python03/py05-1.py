47c3c5ae53bbd005a44788f237dbd0eee0079776
ehi <ehi@pfb04.cshl.edu>
2018-10-17 20:45:44 -0400

    added answers to py05 ps 1-9

---
+++ PFB_problemsets/Python03/py05-1.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+import sys
+
+fav = {'food':'jellybeans', 'book':'No Clue', 'organism':'Nematostella'}
+
+print(fav['book'])
+
+input(fav)
+
+
+for thing in fav:
+	print(thing,fav[thing])
+
+
+
+
