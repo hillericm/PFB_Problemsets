f84c60d1437987e831f28aeb28441089cf3b02f9
ehi <ehi@pfb04.cshl.edu>
2018-10-21 12:15:30 -0400

    added ps10 q4-6 answers

---
+++ PFB_problemsets/Python06/ps10-q4.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+
+class dna_entry(object):
+
+	#define class attributes
+	def __init__(self,gene_name, sequence, gene_species):
+		#allow class attributes to be defined from outside the class
+		self.gene_name = gene_name
+		self.sequence = sequence
+		self.gene_species = gene_species
+
+	#define length method
+	def length(self):
+		length = len(self.sequence)
+		return length
+
+
+dna_entry_obj = dna_entry(gene_name = 'notum', gene_species = 'Homo sapiens', sequence = 'AGTGTGTATGATAGTA')
+
+print(dna_entry_obj)
+print(dna_entry_obj.gene_name)
+print(dna_entry_obj.sequence)
+print(dna_entry_obj.gene_species)
+print(dna_entry_obj.length())
