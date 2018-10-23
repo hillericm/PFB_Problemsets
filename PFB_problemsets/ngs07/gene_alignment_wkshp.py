3c1b522f411b82aad57fc76636e1c95ce499a0c4
ehi <ehi@pfb04.cshl.edu>
2018-10-22 23:13:33 -0400

    cleaned up gene_alignment_wkshp.py

--- PFB_problemsets/ngs07/gene_alignment_wkshp.py
+++ PFB_problemsets/ngs07/gene_alignment_wkshp.py
@@ -11,7 +11,7 @@ for entry in fasta_dict:
 	fasta_length_list.append(len(str(fasta_dict[entry].seq)))
 
 sorted_length_list = sorted(fasta_length_list)[::-1]
-print(sorted_length_list)
+#print(sorted_length_list)
 
 genome_size = 4600000
 sorted_for_N50 = []
@@ -22,14 +22,17 @@ for entry in sorted_length_list:
 
 n50 = sorted_for_N50[-1]
 
-print(sorted_length_list)
-print(sorted_for_N50)
-print(len(sorted_for_N50))
-print(n50)
-print(type(n50))
-print(max(fasta_length_list))
-print(min(fasta_length_list))
-print(type(fasta_length_list))
+#print(sorted_length_list)
+#print(sorted_for_N50)
+print('Total Contig Number', len(fasta_length_list))
+print('Max Contig Length:', max(fasta_length_list))
+print('Min Contig Length:', min(fasta_length_list))
+print('L50:', len(sorted_for_N50))
+print('N50:', n50)
+#print(type(n50))
+#print(max(fasta_length_list))
+#print(min(fasta_length_list))
+#print(type(fasta_length_list))
 #fasta_dict_keys = fasta_dict.keys()
 #print(fasta_length_list)
 #print(fasta_dict)
