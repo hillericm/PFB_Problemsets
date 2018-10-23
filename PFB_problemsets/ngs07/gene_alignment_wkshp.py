690b9b201a6c59019b7d52da90713c758881fd02
ehi <ehi@pfb04.cshl.edu>
2018-10-22 23:08:46 -0400

    added final genome assembly answers

--- PFB_problemsets/ngs07/gene_alignment_wkshp.py
+++ PFB_problemsets/ngs07/gene_alignment_wkshp.py
@@ -1,5 +1,39 @@
 #!/usr/bin/env python3
 
-openfile = open('ecoli_pacbio_0.5.fasta', 'r')
+from Bio import SeqIO
 
+fasta_dict = SeqIO.to_dict(SeqIO.parse('pacbio-0_5.fasta', 'fasta'))
 
+fasta_length_list = []
+
+for entry in fasta_dict:
+#	fasta_length_list.append([fasta_dict[entry].id, len(str(fasta_dict[entry].seq))])	
+	fasta_length_list.append(len(str(fasta_dict[entry].seq)))
+
+sorted_length_list = sorted(fasta_length_list)[::-1]
+print(sorted_length_list)
+
+genome_size = 4600000
+sorted_for_N50 = []
+for entry in sorted_length_list:
+	if genome_size >= 2300000:
+		genome_size -= entry
+		sorted_for_N50.append(entry)
+
+n50 = sorted_for_N50[-1]
+
+print(sorted_length_list)
+print(sorted_for_N50)
+print(len(sorted_for_N50))
+print(n50)
+print(type(n50))
+print(max(fasta_length_list))
+print(min(fasta_length_list))
+print(type(fasta_length_list))
+#fasta_dict_keys = fasta_dict.keys()
+#print(fasta_length_list)
+#print(fasta_dict)
+#print(fasta_dict_keys)
+#print(len(fasta_dict))
+#print(len(fasta_dict.keys()))
+	
